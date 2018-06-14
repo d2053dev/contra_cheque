dc7d6904e9c2941b2a815fe62a1a8eff2709fa56
Doug2053 <dg.silva@outlook.com>
2018-06-13 23:05:20 -0300

    alt mac

--- public/contra_cheque.php
+++ public/contra_cheque.php
@@ -14,15 +14,6 @@ and open the template in the editor.
         <?php
         // put your code here
         ?>
-<<<<<<< HEAD
-        Alteração feita no Mac!
-=======
-        Teste GitHub
-        teste 2
-<<<<<<< HEAD
-=======
-        Windows
->>>>>>> 8c818db45c6c2a2507a1dab5b7780773368b9705
->>>>>>> 3c60f866d11b47c956cd526668158163d1cf6dea
+
     </body>
 </html>
