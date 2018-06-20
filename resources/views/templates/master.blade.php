52ac8c142a3346b5462d37958022db5475f730ba
Doug2053 <dg.silva@outlook.com>
2018-06-20 00:25:52 -0300

    dashboard menu

---
+++ resources/views/templates/master.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Dashboard</title>
+    @yield('css-view')
+    <link rel="stylesheet" href="{{ asset('css/stylesheet.css') }}">
+    <link href="https://fonts.googleapis.com/css?family=Kanit" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+  </head>
+
+  <body>
+      @include('templates.menu-lateral')
+      @yield('conteudo-view')
+      @yield('js-view')
+  </body>
+
+</html>
---
+++ resources/views/templates/menu-lateral.blade.php
@@ -0,0 +1,31 @@
+<nav id="menu">
+  <ul>
+    <li>
+      <a href="#">
+        <i class="fas fa-calculator"></i></i>
+        <h3>Cálculo de Recisão</h3>
+      </a>
+    </li>
+
+    <li>
+      <a href="#">
+        <i class="fas fa-clipboard-list"></i></i>
+        <h3>Folha de Pagamento</h3>
+      </a>
+    </li>
+
+    <li>
+      <a href="#">
+        <i class="fas fa-user-tie"></i></i>
+        <h3>Clientes</h3>
+      </a>
+    </li>
+
+    <li>
+      <a href="#">
+        <i class="fas fa-database"></i></i>
+        <h3>Dados de Cálculo</h3>
+      </a>
+    </li>
+  </ul>
+</nav>
